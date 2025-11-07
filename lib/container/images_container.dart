596e5a4b455fcb9b8a0d745c68b72109eec17b57
Varya <varya.example.com>
2025-11-07 23:23:24 +0300

    Inherited widget added

---
+++ lib/container/images_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../services/images_service.dart';
+
+class ImagesContainer extends InheritedWidget {
+  final ImagesService imagesService;
+
+  const ImagesContainer({
+    Key? key,
+    required this.imagesService,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  static ImagesContainer of(BuildContext context) {
+    final ImagesContainer? result =
+    context.dependOnInheritedWidgetOfExactType<ImagesContainer>();
+    assert(result != null, 'No ImagesContainer found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(ImagesContainer oldWidget) {
+    return imagesService != oldWidget.imagesService;
+  }
+}
