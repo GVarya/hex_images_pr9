c065829769e8c25afc74a2ee164fe6474a76a37a
Varya <varya.example.com>
2025-12-13 14:43:07 +0300

    riverpod logic updated

--- lib/providers/auth_operations.dart
+++ lib/providers/auth_operations.dart
@@ -1,38 +1,3 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-//
-// import '../model/user.dart';
-// import 'providers.dart';
-//
-// class AuthOperations {
-//   final Ref ref;
-//
-//   AuthOperations(this.ref);
-//
-//   void login(String username, String password) {
-//     final user = User(
-//       id: '1',
-//       username: username,
-//       createdAt: DateTime.now(),
-//     );
-//     ref.read(currentUserProvider.notifier).state = user;
-//   }
-//
-//   void register(String username, String password) {
-//     final user = User(
-//       id: '1',
-//       username: username,
-//       createdAt: DateTime.now(),
-//     );
-//     ref.read(currentUserProvider.notifier).state = user;
-//   }
-//
-//   void logout() {
-//     ref.read(currentUserProvider.notifier).state = null;
-//     ref.read(imagesListProvider.notifier).clearImages();
-//   }
-// }
-
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../model/user.dart';
